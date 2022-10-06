ad7f8bf2226503501e688f1ee5e506169b635f2f
João Ygor Ramalho <82791430+joaoygorr@users.noreply.github.com>
2022-10-06 17:18:18 -0400

    components
    Label input + button + select + card + navbar + menuNavbar

---
+++ src/app/components/label-input/label-input.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-label-input',
+  templateUrl: './label-input.component.html',
+})
+export class LabelInputComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ src/app/components/navbar/menu-navbar/menu-navbar.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-menu-navbar',
+  templateUrl: './menu-navbar.component.html',
+})
+export class MenuNavbarComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
