4a43244f004d21df36a5f10bc7520726221cd49c
João Ygor <ygorvieira2002@gmail.com>
2022-10-11 14:50:25 -0400

    remove

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,26 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { NavbarComponent } from './components/navbar/navbar.component';
-import { ManuNavbarComponent } from './components/navbar/manu-navbar/manu-navbar.component';
-import { CardComponent } from './components/card/card.component';
-import { FormMangaComponent } from './components/form-manga/form-manga.component';
-import { FormAuthorComponent } from './components/form-author/form-author.component';
-import { SharedModule } from './shared/shared.module';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent,
-    ManuNavbarComponent,
-    CardComponent,
-    FormMangaComponent,
-    FormAuthorComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    SharedModule
   ],
   providers: [],
   bootstrap: [AppComponent]
