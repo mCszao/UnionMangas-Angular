748ed05055136fdfc63f4992eeaf82bf19b5c673
João Ygor <ygorvieira2002@gmail.com>
2022-10-13 19:36:28 -0400

    app login

--- src/app/modules/home/home.module.ts
+++ src/app/modules/home/home.module.ts
@@ -1,6 +1,4 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-
+import { LoginComponent } from './../../core/login/login.component';
 import { HomeRoutingModule } from './home-routing.module';
 import { HomeComponent } from './home.component';
 import { NavbarComponent } from 'src/app/core/components/navbar/navbar.component';
@@ -8,6 +6,8 @@ import { NavMenuComponent } from 'src/app/core/components/navbar/nav-menu/nav-me
 import { MangaComponent } from 'src/app/core/mangas/manga/manga.component';
 import { ListMangaComponent } from './../../core/mangas/list-manga/list-manga.component';
 
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @NgModule({
   declarations: [
@@ -15,7 +15,8 @@ import { ListMangaComponent } from './../../core/mangas/list-manga/list-manga.co
     NavbarComponent,
     NavMenuComponent,
     ListMangaComponent,
-    MangaComponent
+    MangaComponent,
+    LoginComponent
   ],
   imports: [
     CommonModule,
