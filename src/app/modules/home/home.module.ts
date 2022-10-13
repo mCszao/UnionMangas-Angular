a8ee0500ea727fb29c76413b6bf015c38c430911
João Ygor <ygorvieira2002@gmail.com>
2022-10-13 10:06:51 -0400

    home module

---
+++ src/app/modules/home/home.module.ts
@@ -0,0 +1,20 @@
+import { NavbarComponent } from './../components/navbar/navbar.component';
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { HomeRoutingModule } from './home-routing.module';
+import { HomeComponent } from './home.component';
+
+
+@NgModule({
+  declarations: [
+    HomeComponent,
+    NavbarComponent
+  ],
+  imports: [
+    CommonModule,
+    HomeRoutingModule,
+    
+  ]
+})
+export class HomeModule { }
