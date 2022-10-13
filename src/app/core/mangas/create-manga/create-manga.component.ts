be607be9673698ada22ce1e20f849e0e51b8e2ea
João Ygor <ygorvieira2002@gmail.com>
2022-10-13 14:51:01 -0400

    remove styles

--- src/app/core/mangas/create-manga/create-manga.component.ts
+++ src/app/core/mangas/create-manga/create-manga.component.ts
@@ -3,7 +3,6 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-create-manga',
   templateUrl: './create-manga.component.html',
-  styleUrls: ['./create-manga.component.css']
 })
 export class CreateMangaComponent implements OnInit {
 
