aa90076d3dd825e84def2b858da681e52a41d86a
João Ygor <ygorvieira2002@gmail.com>
2022-10-13 10:37:14 -0400

    imports

--- src/app/modules/home/home.module.ts
+++ src/app/modules/home/home.module.ts
@@ -1,10 +1,10 @@
-import { NavMenuComponent } from './../components/navbar/nav-menu/nav-menu.component';
-import { NavbarComponent } from './../components/navbar/navbar.component';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { HomeRoutingModule } from './home-routing.module';
 import { HomeComponent } from './home.component';
+import { NavbarComponent } from 'src/app/core/components/navbar/navbar.component';
+import { NavMenuComponent } from 'src/app/core/components/navbar/nav-menu/nav-menu.component';
 
 
 @NgModule({
