f842d7b48bb8d57bca3166d4d404bc43875a92cb
João Ygor <ygorvieira2002@gmail.com>
2022-10-19 18:27:06 -0400

    remove imports

--- src/app/modules/home/home.module.ts
+++ src/app/modules/home/home.module.ts
@@ -3,8 +3,6 @@ import { HomeRoutingModule } from './home-routing.module';
 import { HomeComponent } from './home.component';
 import { NavbarComponent } from 'src/app/core/components/navbar/navbar.component';
 import { NavMenuComponent } from 'src/app/core/components/navbar/nav-menu/nav-menu.component';
-import { MangaComponent } from 'src/app/core/mangas/manga/manga.component';
-import { ListMangaComponent } from './../../core/mangas/list-manga/list-manga.component';
 import { RegisterUserComponent } from 'src/app/core/register-user/register-user.component';
 
 import { NgModule } from '@angular/core';
@@ -16,8 +14,6 @@ import { FormsModule } from '@angular/forms';
     HomeComponent,
     NavbarComponent,
     NavMenuComponent,
-    ListMangaComponent,
-    MangaComponent,
     LoginComponent,
     RegisterUserComponent
   ],
