b8def523056b84565bab5794ffc3d1624f65a39d
João Ygor <ygorvieira2002@gmail.com>
2022-10-19 18:35:54 -0400

    module mangas + routing

---
+++ src/app/modules/mangas/mangas.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { MangasRoutingModule } from './mangas-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    MangasRoutingModule
+  ]
+})
+export class MangasModule { }
