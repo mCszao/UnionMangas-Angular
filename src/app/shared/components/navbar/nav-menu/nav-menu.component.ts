1902ab7336e7aafa1834060da020f096e9f13655
João Ygor <ygorvieira2002@gmail.com>
2022-10-31 16:34:05 -0400

    ajuster Login/register + navbar

--- src/app/shared/components/navbar/nav-menu/nav-menu.component.ts
+++ src/app/shared/components/navbar/nav-menu/nav-menu.component.ts
@@ -3,6 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-nav-menu',
   templateUrl: './nav-menu.component.html',
+  styleUrls: ['./nav-menu.component.css']
 })
 export class NavMenuComponent implements OnInit {
   display: boolean = false; 
