234ffd161fba9895d6d8f0545dc3a114ffabb2dc
João Ygor <ygorvieira2002@gmail.com>
2022-11-08 15:56:37 -0400

    routes and component

--- src/app/modules/mangas/mangas-routing.module.ts
+++ src/app/modules/mangas/mangas-routing.module.ts
@@ -1,3 +1,5 @@
+import { MangaComponent } from './manga/manga.component';
+import { RegisterMangaComponent } from './register-manga/register-manga.component';
 import { ListMangaComponent } from './list-manga/list-manga.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
@@ -6,6 +8,14 @@ const routes: Routes = [
   {
     path: "",
     component: ListMangaComponent
+  },
+  {
+    path: "registerManga",
+    component: RegisterMangaComponent
+  },
+  {
+    path: "manga",
+    component: MangaComponent
   }
 ];
 
