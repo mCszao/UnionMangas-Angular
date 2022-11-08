4cbf55456dc5d3d3e1a827034d1214d494cfb069
João Ygor <ygorvieira2002@gmail.com>
2022-11-08 15:57:07 -0400

    edit manga

---
+++ src/app/modules/mangas/edit-manga/edit-manga.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { EditMangaComponent } from './edit-manga.component';
+
+describe('EditMangaComponent', () => {
+  let component: EditMangaComponent;
+  let fixture: ComponentFixture<EditMangaComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ EditMangaComponent ]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(EditMangaComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/modules/mangas/edit-manga/edit-manga.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-edit-manga',
+  templateUrl: './edit-manga.component.html',
+  styleUrls: ['./edit-manga.component.css']
+})
+export class EditMangaComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
