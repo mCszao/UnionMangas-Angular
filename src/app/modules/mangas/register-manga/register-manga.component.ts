83f462f06b8e8aaee7da7c461a80d418edaa3ba5
João Ygor <ygorvieira2002@gmail.com>
2022-11-10 18:06:53 -0400

    chamando serviço categories

--- src/app/modules/mangas/register-manga/register-manga.component.ts
+++ src/app/modules/mangas/register-manga/register-manga.component.ts
@@ -1,3 +1,5 @@
+import { ICategories } from './../../../shared/interface/ICategories';
+import { CategoiresService } from './../../../shared/services/categoires/categoires.service';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -6,10 +8,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./register-manga.component.css']
 })
 export class RegisterMangaComponent implements OnInit {
-  cities!: any[];
-  constructor() { }
+  categoires: ICategories[] = [];
+
+  constructor(private categoiresService: CategoiresService) { }
 
   ngOnInit(): void {
+    this.categoiresService.findAllCategory().subscribe(response => {
+      this.categoires = response.data;
+      console.log(response.data.map(teste => {
+        console.log(teste.name[0].toUpperCase() + teste.name.slice(1).toLowerCase());
+        
+      }));
+    })
   }
 
 }
