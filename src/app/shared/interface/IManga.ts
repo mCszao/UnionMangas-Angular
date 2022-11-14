bed4831aa331c21b5e21fa95eca823e5032bc4e1
João Ygor <ygorvieira2002@gmail.com>
2022-11-14 17:10:07 -0400

    or undefined

--- src/app/shared/interface/IManga.ts
+++ src/app/shared/interface/IManga.ts
@@ -2,17 +2,17 @@ import { ICategories } from './ICategories';
 import { IAuthor } from "./IAuthor";
 
 export interface IManga {
-    idManga: number,
+    idManga?: number,
     mainTitle: string,
-    alternativeTitle: string,
+    alternativeTitle?: string,
     linkImage: string,
-    blobImage: string[],
+    blobImage?: string[],
     description: string,
-    status: string,
-    rating: number,
-    volumeQuantity: number,
-    releaseDate: Date,
-    lastUpdate: Date,
+    status?: string,
+    rating?: number,
+    volumeQuantity?: number,
+    releaseDate?: Date,
+    lastUpdate?: Date,
     authors: IAuthor[],
     categories: ICategories[]
 }
