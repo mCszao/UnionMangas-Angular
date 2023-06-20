2ea75ff37f1de1096b564f5f0157b6d55d8a7574
joaoygorr <ygorvieira2002@gmail.com>
2023-06-19 22:58:17 -0400

    build: update primeNg

--- src/app/modules/authenticate/guards/auth.guard.ts
+++ src/app/modules/authenticate/guards/auth.guard.ts
@@ -1,12 +1,12 @@
 import { environment } from 'src/environments/environment';
 import { LoginService } from './../../../shared/services/login/login.service';
 import { Injectable } from '@angular/core';
-import { CanActivate, Router } from '@angular/router';
+import { Router } from '@angular/router';
 
 @Injectable({
   providedIn: 'root'
 })
-export class AuthGuard implements CanActivate {
+export class AuthGuard  {
   constructor(
     private router: Router,
     private loginService: LoginService
